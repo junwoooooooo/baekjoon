6b7a792d3c2f210c2af91f84cf34740df3c38393
junwoooooooo <hjwoo02233@naver.com>
2026-04-08 10:27:46 +0900

    [Gold V] Title: 토마토, Time: 484 ms, Memory: 213136 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,41 +1,42 @@
 from collections import deque
-m, n = map(int, input().split())      
-tmtmap = []                          
+
+m,n = map(int,input().split())
+tmtmap = []
 find = deque()
 
-for i in range(n):                   
-    tmtlist = list(map(int, input().split()))
-    tmtmap.append(tmtlist)
+for i in range(n):
+    tmtlis =list(map(int,input().split()))
+    tmtmap.append(tmtlis)
     
-for i in range(n):                    
+for i in range(n):
     for j in range(m):
         if tmtmap[i][j] == 1:
-            find.append([i, j])      
-            
+            find.append([i,j])
+        
 dxy = [[0,1],[1,0],[0,-1],[-1,0]]
 
-def tmt():
-    while find:                       
-        i, j = find.popleft()        
-        for k in range(4):            
-            dx = dxy[k][0]
-            dy = dxy[k][1]
-            if 0 <= dx+i < n and 0 <= dy+j < m and tmtmap[dx+i][dy+j] == 0: 
-                tmtmap[dx+i][dy+j] = tmtmap[i][j] + 1    
-                find.append([dx+i, dy+j])
+def tmt():    
+    while find:
+        i, j = find.popleft()
+        for k in range(4):
+            ni = dxy[k][0] + i
+            nj = dxy[k][1] + j
+            
+            if 0 <= ni < n and 0 <= nj < m and tmtmap[ni][nj] == 0 :
+                tmtmap[ni][nj] = tmtmap[i][j] + 1
+                find.append([ni , nj])
+                
 tmt()
-
-for row in tmtmap:
-    if 0 in row:
+maxnum = 0
+for i in range(n):
+    if 0 in tmtmap[i]:
         print(-1)
         break
+    for j in range(m):         
+        maxnum = max(maxnum, tmtmap[i][j])
 else:
-    print(max(max(row) for row in tmtmap) - 1)
-
-
-
-
-
-
-    
-    
+    print(maxnum - 1)
+        
+        
+        
+       
