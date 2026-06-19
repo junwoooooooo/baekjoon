8d390aab1c2b6b8debb42bf8a6ae2b5544120361
junwoooooooo <hjwoo02233@naver.com>
2026-06-19 15:32:20 +0900

    [level 3] Title: 베스트앨범, Time: 0.14 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -2,22 +2,23 @@ def solution(genres, plays):
     gp = {}
     
     for i in range(len(genres)):
-        p = genres[i]
-        gp.setdefault(p , [])
-        gp[p].append([plays[i], i])
+        g = genres[i]
+        gp.setdefault(g,[])
+        gp[g].append([plays[i] , i])
         
-    s_genres = {}
+    g_sum = {}
     for g in gp:
-        s_genres[g] = sum(x[0]for x in gp[g])
+        g_sum[g] = sum(x[0] for x in gp[g] )
         
     for g in gp:
-        gp[g].sort(key=lambda x: (-x[0], x[1]))
-        
-    s_genres_sort = sorted(s_genres, key=lambda x: -s_genres[x])
+        gp[g].sort(key = lambda x: (-x[0],x[1]))
+    
+    sort_genres = sorted(g_sum , key = lambda x: -g_sum[x])
     
     answer = []
-    for p in s_genres_sort:
-        for x in gp[p][:2]:
-            answer.append(x[1])
+    for p in sort_genres:
+        for i in gp[p][:2]:
+            answer.append(i[1])
+    
                 
     return answer
